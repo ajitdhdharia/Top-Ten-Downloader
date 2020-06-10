23385ea9a1c907ca45f36585d6d606964f910bac
Ajit Dhdharia <ajitdhdhariaaa@gmail.com>
2020-06-10 17:44:41 -0400

    ListView Added.

--- app/src/main/java/com/example/top10downloader/MainActivity.java
+++ app/src/main/java/com/example/top10downloader/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.top10downloader;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -19,18 +24,54 @@ import java.nio.Buffer;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private ListView listApps;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listApps = findViewById(R.id.xmlListView);
 
-        Log.d(TAG, "onCreate: AsyncTask is executing");
-        DownloadData downloadData = new DownloadData();
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml"); //This URL goes into doInBackground method of the AsyncTask.
-        Log.d(TAG, "onCreate: AsyncTask is done");
+       downloadUrl("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.feeds_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        String feedUrl;
+
+        switch(id) {
+            case R.id.mnuFree:
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml";
+                break;
+            case R.id.mnuPaid:
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=10/xml";
+                break;
+            case R.id.mnuSongs:
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=10/xml";
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        downloadUrl(feedUrl);
+        return true;
+    }
+
+    private void downloadUrl(String feedUrl){
+        Log.d(TAG, "downloadUrl: AsyncTask is executing");
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute(feedUrl); //This URL goes into doInBackground method of the AsyncTask.
+        Log.d(TAG, "downloadUrl: AsyncTask is done");
+
+    }
 
     // Methods of this AsyncTask are executed on seperate Thread.
     private class DownloadData extends AsyncTask<String, Void, String> {
@@ -41,7 +82,12 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(s);
             ParseApplications parseApplications = new ParseApplications();
             parseApplications.parse(s);
-            Log.d(TAG, "onPostExecute: parameter is " + s);
+
+//            ArrayAdapter<FeedEntry> arrayAdapter = new ArrayAdapter<FeedEntry>(MainActivity.this, R.layout.list_item, parseApplications.getApplications());
+//            listApps.setAdapter(arrayAdapter);
+
+            FeedAdapter feedAdapter = new FeedAdapter(MainActivity.this, R.layout.list_record, parseApplications.getApplications());
+            listApps.setAdapter(feedAdapter);
         }
 
         @Override
